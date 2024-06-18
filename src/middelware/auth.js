c1d80599c01ebca11a13ced9ace8e78ad336d7be
omnia334 <110954551+omnia334@users.noreply.github.com>
2024-06-18 06:43:57 +0300

    Add files via upload

--- src/middelware/auth.js
+++ src/middelware/auth.js
@@ -13,5 +13,25 @@ const user = async (req, res, next) => {
         res.send(e.message)
     }
 }
+// Example of an authentication middleware
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '');
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
 
-module.exports = { user }
+        if (!user) {
+            throw new Error();
+        }
+
+        req.token = token;
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(401).send({ error: 'Please authenticate.' });
+    }
+};
+
+
+
+module.exports = { user,auth }
